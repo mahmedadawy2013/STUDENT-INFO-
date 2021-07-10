afa956d9bbd8b3ae5a4d41c776b84803d4d93a6d
mahmedadawy2013 <75279964+mahmedadawy2013@users.noreply.github.com>
2021-07-10 23:23:50 +0200

    Add files via upload

---
+++ STUDENT 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Џг— Ќ”«г «бѕнд Ё Ќн Џбн sec:3 B.N.:14
+//Џг— г’ЎЁн «Ќгѕ «б‘нгн sec:3 B.N.:11
+//гЌгѕ «Ќгѕ «бЏѕжн sec:3 B.N.:28
+struct StudentInfo
+{
+  char name[25];
+  int ID;
+  int dateofbirth[3];
+  int score;
+} ;
